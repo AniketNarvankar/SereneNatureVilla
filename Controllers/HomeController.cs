843789c9e54beb50c1db158a2d0178b54a4ab7f1
Aniket Narvankar <narvankar.aniket@gmail.com>
2025-11-01 15:41:51 +0530

    Initial commit of Serene Nature Villa project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web.Mvc;
+
+namespace sereneVilla.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs(){
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult PlacesToVisit() {
+            return View();
+        }
+    }
+}
